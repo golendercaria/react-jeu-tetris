9fe226027ca649300901f8cccd889f7857c33b43
golendercaria <>
2021-11-28 16:44:57 +0100

    Update and makge build for github page

---
+++ build/precache-manifest.10546d2a26dfb741ab3bd9e4418575a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9d329620eee917ef6b550082d961365",
+    "url": "./index.html"
+  },
+  {
+    "revision": "baaf147693a6b6cf58b6",
+    "url": "./static/css/main.c510978c.chunk.css"
+  },
+  {
+    "revision": "5eedee7e46ac2f7bae6a",
+    "url": "./static/js/2.429a9f20.chunk.js"
+  },
+  {
+    "revision": "baaf147693a6b6cf58b6",
+    "url": "./static/js/main.4b65f92c.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
